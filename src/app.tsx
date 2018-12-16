d5261975b06339f3041e957f6b258fcfc9d22953
Marcelo Augusto Rodrigues Alves <marceloinvest@gmail.com>
2018-12-16 14:49:15 +0000

    added material-ui lib and it's demo

--- src/app.tsx
+++ src/app.tsx
@@ -3,13 +3,15 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Layout } from "./components/layout";
 import { AsyncDemo } from "./components/AsyncDemo";
+import { FancyDemo } from "./components/FancyDemo";
 
 
 let mainUi = (
     <Layout title="React template">
+        <FancyDemo />
         <AsyncDemo title="Async Demo" delay={2000} />
     </Layout>
-    );
+    ); 
 
 
 ReactDOM.render(mainUi, document.getElementById("root"));
---
+++ src/components/FancyDemo.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { LinearProgress, CircularProgress, Button, Grid, AppBar, TextField, Select } from "@material-ui/core";
+import { element } from "prop-types";
+
+
+export function FancyDemo(){
+
+    var options = ["Absolute, Beam, Car, Director, Errant, Fox, Good, Hello, Intel, Jack"];
+
+    return (
+        <div>
+            <AppBar color="primary" position="fixed">
+                <h1>Fancy components</h1>
+            </AppBar>
+            <hr/>
+            <Grid container >
+                <Grid xs={2}>
+                    <Select native value={0} >
+                        <option value="" />
+                        <option value={0}>Foo</option>
+                        <option value={1}>Bar</option>
+                        <option value={2}>FooBar</option>
+                    </Select>
+                </Grid>
+                <Grid xs={2}>
+                    <TextField type="time" />
+                </Grid>
+                <Grid xs={2}>
+                    <Button variant="contained">A Button</Button>
+                </Grid>
+                <Grid xs={2}>
+                    <CircularProgress variant="indeterminate" />
+                </Grid>
+                <Grid xs={2}>
+                    <LinearProgress variant="indeterminate" />
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
