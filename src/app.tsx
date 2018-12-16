448f38b50c8f7f0614d2c4110356fa991995a0f7
Marcelo Augusto Rodrigues Alves <marceloinvest@gmail.com>
2018-12-16 22:43:24 +0000

    refactor file names

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,8 @@ import "typeface-roboto";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Layout } from "./components/layout";
-import { AsyncDemo } from "./components/AsyncDemo";
-import { FancyDemo } from "./components/FancyDemo"; 
+import { AsyncDemo } from "./components/async-demo";
+import { FancyDemo } from "./components/fancy-demo"; 
 
 
 let mainUi = (
