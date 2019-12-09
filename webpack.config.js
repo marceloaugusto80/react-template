4ff422915792608d15a74e283c2855c67ffa71a0
Marcelo Augusto Rodrigues Alves <marceloinvest@gmail.com>
2019-12-09 14:34:16 -0300

    updated dependencies to latest version

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackExternalsPlugin = require("html-webpack-externals-plugin");
 
